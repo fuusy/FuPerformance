0542d4bab0fb512543bf1af51bd8a762e698a22f
fuusy <643432857@qq.com>
2021-08-22 21:21:55 +0800

    add memory activity

--- app/src/main/java/com/fuusy/fuperformance/MainActivity.kt
+++ app/src/main/java/com/fuusy/fuperformance/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.fuusy.fuperformance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.fuusy.fuperformance.memory.MemoryActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.bt_memory).setOnClickListener {
+            Intent(this, MemoryActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/fuusy/fuperformance/memory/MemoryActivity.kt
@@ -0,0 +1,19 @@
+package com.fuusy.fuperformance.memory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fuusy.fuperformance.R
+
+/**
+ * @date：2021/8/22
+ * @author fuusy
+ * @instruction：内存优化
+ */
+class MemoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memory)
+
+
+    }
+}
